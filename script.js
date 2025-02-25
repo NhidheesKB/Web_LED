c17248b8835b001592822e0985a58d5acbc28148
NhidheesKB <nhidhees@gmail.com>
2025-02-25 17:21:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleComputer = document.getElementById("toggleComputer");
+    const valueBox = document.getElementById("valueBox");
+    const counterBox = document.getElementById("counterBox");
+    let counter=0;
+
+    function updateValue() {
+        valueBox.textContent = toggleComputer.checked ? "ON" : "OFF";
+    }
+
+    toggleComputer.addEventListener("click", function() {
+        updateValue();
+    });
+    function incrementCounter() {
+        if (counter <= 255) {
+            counterBox.textContent = `${counter}V`;
+            counter++;
+        } else {
+            counter = 0; 
+        }
+    }
+    setInterval(incrementCounter, 2000);
+
+    
+    
+    
+    // Initialize value box state
+    updateValue();
+});
