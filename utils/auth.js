6eed91597cbb6e479111556a51cbe8f311fd580b
k.b.Yudhees Raam Kumar <yudees.raamkumar@bakuun.com>
2025-03-02 20:16:24 +0530

    Performance fixture

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,9 @@
-import { app} from "./firebase.js";
+import { getApp} from "./firebase.js";
 import { getAuth,signInWithEmailAndPassword} from "firebase/auth";
-const auth = getAuth(app);
 export const login=async(email,password)=>{
     let errorCode,errorMessage;
+    const app=getApp()
+    const auth = getAuth(app);
    try {
       const res=await signInWithEmailAndPassword(auth,email,password)
       const token = await res.user.getIdToken();
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,14 +1,16 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-    apiKey: process.env.FIRE_BASE_API_KEY,
-    authDomain:process.env.FIRE_BASE_DOMAIN ,
-    projectId:process.env.FIRE_BASE_PROJECT_ID ,
-    storageBucket:process.env.FIRE_BASE_STORAGE_BUCKET ,
-    messagingSenderId:process.env.FIRE_BASE_MESSAGE_SENDER_ID ,
-    appId:process.env.FIRE_BASE_APP_ID ,
-    // measurementId:process.env.FIRE_BASE_MEASUREMENT_ID 
-};
-
-const app = initializeApp(firebaseConfig);
-export{app}
+const getApp=()=>{
+    const firebaseConfig = {
+        apiKey: process.env.FIRE_BASE_API_KEY,
+        authDomain:process.env.FIRE_BASE_DOMAIN ,
+        projectId:process.env.FIRE_BASE_PROJECT_ID ,
+        storageBucket:process.env.FIRE_BASE_STORAGE_BUCKET ,
+        messagingSenderId:process.env.FIRE_BASE_MESSAGE_SENDER_ID ,
+        appId:process.env.FIRE_BASE_APP_ID ,
+        // measurementId:process.env.FIRE_BASE_MEASUREMENT_ID 
+    };
+    const app = initializeApp(firebaseConfig);
+    return app
+}
+export{getApp}
